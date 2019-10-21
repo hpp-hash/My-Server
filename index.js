4e940616644cc03d9aa0d594b9170a611a1f8b9c
Hung Phan <17309881+hp0101@users.noreply.github.com>
2019-10-20 22:50:01 -0700

    static files in 'public' folder will be served over port 3000, nginx re-routes port 80 to port 3000

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+// static files in 'public' folder will be served
 app.use(express.static('public'))
 
 app.listen(3000, () => console.log('Server running on port 3000'))
